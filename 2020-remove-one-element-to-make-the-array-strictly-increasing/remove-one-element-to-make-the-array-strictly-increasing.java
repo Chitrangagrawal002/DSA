c742bcea925fb4a8029957f3c95b457c383e1b62
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-01-21 22:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (13.79%) - LeetSync

--- 2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.java
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.java
@@ -1,25 +1,13 @@
 class Solution {
     public boolean canBeIncreasing(int[] nums) {
         int count = 0;
-        int peak = 0;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] > nums[i+1] || nums[i] == nums[i+1]){
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
                 count++;
-                peak = i;
-            }
-        }
-        if(count > 1){
-            return false;
-        }
-        else if(count == 1){
-            if(peak == 0 || peak == nums.length - 2){
-                return true;
-            }
-            if(nums[peak + 1] > nums[peak - 1] || nums[peak] < nums[peak + 2]){
-                return true;
-            }
-            else {
-                return false;
+                if (count > 1) return false;
+                if (i > 1 && nums[i] <= nums[i - 2]) {
+                    nums[i] = nums[i - 1]; 
+                }
             }
         }
         return true;
