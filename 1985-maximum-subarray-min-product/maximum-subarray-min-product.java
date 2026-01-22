248cea80babdac705225d10b12c1fdee05c27bf8
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-01-22 19:29:37 +0530

    Time: 27 ms (92.24%) | Memory: 82.4 MB (93.35%) - LeetSync

--- 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
+++ 1985-maximum-subarray-min-product/maximum-subarray-min-product.java
@@ -1,39 +1,23 @@
 class Solution {
     public int maxSumMinProduct(int[] nums) {
         int n = nums.length;
-        long mod = 1_000_000_007;
-        long prefix[] = new long[nums.length + 1];
-        int pse[] = new int[n];
-        int nse[] = new int[n];
-        for(int i=0; i<n; i++){
-            prefix[i+1] = prefix[i] + nums[i];
-        }
-        Stack<Integer> st = new Stack<>();
-        Arrays.fill(pse, -1);
-        for(int i=0; i<n; i++){
-            while(!st.isEmpty() && nums[st.peek()] >= nums[i]){
-                st.pop();
-            }
-            pse[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
-        st.clear();
-        Arrays.fill(nse, n);
-        for(int i=n-1; i>=0 ; i--){
-            while(!st.isEmpty() && nums[st.peek()] >= nums[i]){
-                st.pop();
-            }
-            nse[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
-        }
+        long MOD = 1_000_000_007L;
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++) prefix[i + 1] = prefix[i] + nums[i];
+        Deque<Integer> st = new ArrayDeque<>();
         long ans = 0;
-        for(int i=0; i<n; i++){
-            int left = pse[i] + 1;
-            int right = nse[i] - 1;
-            long sum = prefix[right + 1] - prefix[left];
-            long product = sum * nums[i];
-            ans = Math.max(ans, product);
+        for (int i = 0; i <= n; i++) {
+            long cur = (i == n ? 0 : nums[i]);
+            while (!st.isEmpty() && nums[st.peek()] > cur) {
+                int mid = st.pop();
+                int left = st.isEmpty() ? 0 : st.peek() + 1;
+                int right = i - 1;
+
+                long sum = prefix[right + 1] - prefix[left];
+                ans = Math.max(ans, sum * nums[mid]);
+            }
+            st.push(i);
         }
-        return (int)(ans % mod);
+        return (int)(ans % MOD);
     }
 }
