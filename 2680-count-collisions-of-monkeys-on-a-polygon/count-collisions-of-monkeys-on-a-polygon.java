415692e40f5c16a0775bfd90ae1c0af594a3a65e
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-01-22 20:52:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (33.70%) - LeetSync

---
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int monkeyMove(int n) {
+        long ans = 1;
+        long mod = 1_000_000_007;
+        long base = 2;
+        while(n > 0){
+            if((n & 1) == 1) ans = (ans * base) % mod;
+            base = (base * base) % mod;
+            n >>= 1;
+        }
+        return (int)((ans - 2 + mod) % mod);
+    }
+}
