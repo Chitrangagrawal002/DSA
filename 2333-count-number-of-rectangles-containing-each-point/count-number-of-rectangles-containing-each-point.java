5dab31ef9c18be64397bd5d8e0ade52c8d831b8e
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-01-28 22:38:11 +0530

    Time: 253 ms (65.87%) | Memory: 94.1 MB (62.87%) - LeetSync

---
+++ 2333-count-number-of-rectangles-containing-each-point/count-number-of-rectangles-containing-each-point.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] countRectangles(int[][] rectangles, int[][] points) {
+        List<Integer>[] list = new ArrayList[101];
+        for(int i = 1; i < 101; i++){
+            list[i] = new ArrayList<>();
+        }
+        for(int r[] : rectangles){
+            list[r[1]].add(r[0]);
+        }
+        for(int i = 1; i <= 100; i++){
+            Collections.sort(list[i]);
+        }
+        int res[] = new int[points.length];
+        for(int i = 0; i < points.length; i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            int count = 0;
+            for(int j = y; j <= 100; j++){
+                List<Integer> curr = list[j];
+                int idx = lowerbound(curr, x);
+                count += curr.size() - idx;
+            }
+            res[i] = count;
+        }
+        return res;
+    }
+    public int lowerbound(List<Integer> curr, int target) {
+        int lo = 0, hi = curr.size();
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (curr.get(mid) < target) lo = mid + 1;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
