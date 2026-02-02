0800267bfe015e2f34be5da0b8de93cdc401b47f
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-02-02 12:04:01 +0530

    Time: 4 ms (99.26%) | Memory: 42.4 MB (73.76%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
@@ -1,16 +1,16 @@
 class Solution {
-    long mod = 1_000_000_007;
-    public long fibo(long dp[], int n){
-        if(n == 0) return 1;
-        if(n == 1) return 2;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = (fibo(dp, n - 1) + fibo(dp, n - 2)) % mod;
-    }
     public int countHousePlacements(int n) {
-        long dp[] = new long[n + 1];
-        Arrays.fill(dp, -1);
-        long plots = fibo(dp, n);
-        long ans = (plots * plots) % mod;
+        if(n == 1) return 4;
+        long mod = 1_000_000_007;
+        long a = 1;
+        long b = 2;
+        long k = 0;
+        for(int i = 2; i <= n; i++){
+            k = (a + b) % mod;
+            a = b;
+            b = k;
+        }
+        long ans = (k * k) % mod;
         return (int) ans;
     }
 }
