5ee4daaf42be0df00f5a88e593c5ac935d1502ad
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-02-02 12:10:40 +0530

    Time: 4 ms (99.26%) | Memory: 42.1 MB (95.79%) - LeetSync

--- 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
@@ -1,16 +1,14 @@
 class Solution {
     public int countHousePlacements(int n) {
-        if(n == 1) return 4;
         long mod = 1_000_000_007;
         long a = 1;
         long b = 2;
-        long k = 0;
-        for(int i = 2; i <= n; i++){
-            k = (a + b) % mod;
+        for (int i = 2; i <= n; i++) {
+            long k = (a + b) % mod;
             a = b;
             b = k;
         }
-        long ans = (k * k) % mod;
+        long ans = (b * b) % mod;
         return (int) ans;
     }
 }
