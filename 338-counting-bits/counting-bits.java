19eaf27aaf6e732898513e43bbaccc1f15db42fc
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-02-15 20:37:25 +0530

    Time: 2 ms (96.58%) | Memory: 48.6 MB (69.65%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        if(n == 0) return new int[]{0};
+        if(n == 1) return new int[]{0, 1};
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = Integer.bitCount(i);
+        }
+        return dp;
+    }
+}
