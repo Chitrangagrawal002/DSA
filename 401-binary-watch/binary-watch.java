1a9207e9c127fa0a1422ac2a2cd7551477c82299
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-02-17 11:07:25 +0530

    Time: 5 ms (83.63%) | Memory: 49.5 MB (21.98%) - LeetSync

---
+++ 401-binary-watch/binary-watch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<>();
+        for(int i=0; i<=11; i++){
+            for(int j=0; j<=59; j++){
+                if(Integer.bitCount(i) + Integer.bitCount(j) == turnedOn){
+                    ans.add(i + ":" + (j < 10 ? "0" + j : j));
+                }
+            }
+        }
+        return ans;
+    }
+}
