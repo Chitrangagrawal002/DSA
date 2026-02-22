cdd60dbda8b80692823cfefe46a96ac94b383140
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-02-22 21:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (48.75%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,19 +1,17 @@
 class Solution {
     public int binaryGap(int n) {
-        int arr[] = new int[32];
-        int idx = 0;
-        int count = 0;
+        int prev = -1;
+        int ans = 0;
+        int curr = 0;
         while(n > 0){
             if((n & 1) == 1){
-                arr[idx] = count;
-                idx++;
+                if (prev != -1){
+                    ans = Math.max(ans, curr - prev);
+                }
+                prev = curr;
             }
             n >>= 1;
-            count++;
-        }
-        int ans = 0;
-        for(int i = 1; i < arr.length; i++){
-            ans = Math.max(ans, arr[i] - arr[i - 1]);
+            curr++;
         }
         return ans;
     }
