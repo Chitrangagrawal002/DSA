5d572a52e4d064027c9278b38074346246db277a
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-02-23 22:50:15 +0530

    Time: 1 ms (89.79%) | Memory: 43.6 MB (9.57%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.java
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -1,11 +1,18 @@
 class Solution {
     public int maxRepeating(String sequence, String word) {
-        int ans = 0;
-        String curr = word;
-        while(sequence.contains(curr)){
-            ans++;
-            curr += word;
+        int n = sequence.length();
+        int m = word.length();
+        int max = 0;
+        for(int i = 0; i <= n - m; i++) {
+            int count = 0;
+            int start = i;
+            while(start + m <= n && 
+                  sequence.substring(start, start + m).equals(word)) {
+                count++;
+                start += m;
+            }
+            max = Math.max(max, count);
         }
-        return ans;
+        return max;
     }
 }
