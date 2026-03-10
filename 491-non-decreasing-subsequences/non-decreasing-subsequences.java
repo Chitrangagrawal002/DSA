9835de9764dbca4e53c5f80c0b4025bd248e825c
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-03-10 16:19:59 +0530

    Time: 7 ms (65.37%) | Memory: 52.8 MB (79.44%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -1,26 +1,22 @@
 class Solution {
-    Set<String> set = new HashSet<>();
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> findSubsequences(int[] nums) {
-        helper(nums, new ArrayList<Integer>(), 0, -200);
+        helper(nums, new ArrayList<Integer>(), 0);
         return res;
     }
-    public void helper(int[] nums, List<Integer> curr, int i, int prev){
+    public void helper(int[] nums, List<Integer> curr, int start){
         if(curr.size() >= 2){
-            String s = curr.toString();
-            if(!set.contains(s)){
-                res.add(new ArrayList<>(curr));
-                set.add(s);
+            res.add(new ArrayList<>(curr));
+        }
+        Set<Integer> used = new HashSet<>();
+        for(int i = start; i < nums.length; i++){
+            if(used.contains(nums[i])) continue;
+            if(curr.size() == 0 || nums[i] >= curr.get(curr.size() - 1)){
+                used.add(nums[i]);
+                curr.add(nums[i]);
+                helper(nums, curr, i + 1);
+                curr.remove(curr.size() - 1);
             }
         }
-        if(i >= nums.length){
-            return;
-        }
-        if(nums[i] >= prev){
-            curr.add(nums[i]);
-            helper(nums, curr, i + 1, nums[i]);
-            curr.remove(curr.size() - 1);
-        }
-        helper(nums, curr, i + 1, prev);
     }
 }
