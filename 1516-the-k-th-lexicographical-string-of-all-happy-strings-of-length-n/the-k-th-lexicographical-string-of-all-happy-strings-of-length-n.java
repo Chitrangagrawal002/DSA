68548ef5fb3d146a3d9999bac175972e7ff38745
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-03-14 11:05:44 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (96.44%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,21 +1,24 @@
 class Solution {
+    int count = 0;
+    String ans = "";
     public String getHappyString(int n, int k) {
-        List<String> list = new ArrayList<>();
-        dfs(n, list, new StringBuilder());
-        if(list.size() < k) return "";
-        Collections.sort(list);
-        return list.get(k - 1);
+        dfs(n, new StringBuilder(), k);
+        return ans;
     }
-    public void dfs(int n, List<String> list, StringBuilder sb){
+    public void dfs(int n, StringBuilder sb, int k){
         if(n == 0){
-            list.add(sb.toString());
+            count++;
+            if(count == k){
+                ans = sb.toString();
+            }
             return;
         }
         for(char ch = 'a'; ch < 'd'; ch++){
             if(sb.length() > 0 && ch == sb.charAt(sb.length() - 1)){
                 continue;
             }
-            dfs(n - 1, list, sb.append(ch));
+            dfs(n - 1,sb.append(ch), k);
+            if(ans.length() > 0) return;
             sb.deleteCharAt(sb.length() - 1);
         }
     }
