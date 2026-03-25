225704effd4fd147632cd92a64c234b71586fa17
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-03-25 15:12:55 +0530

    Time: 2 ms (70.02%) | Memory: 50.3 MB (27.81%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.java
+++ 923-super-egg-drop/super-egg-drop.java
@@ -1,13 +1,13 @@
 class Solution {
     public int superEggDrop(int k, int n) {
-        int[] dp = new int[k+1];
-        int cnt = 0;
-        while(dp[k] < n){
-            cnt++;
-            for(int i=k; i>=1; i--){
-                dp[i] += dp[i-1] + 1;
+        int dp[][] = new int[k + 1][n + 1];
+        int count = 0;
+        while(dp[k][count] < n){
+            count++;
+            for(int i = 1; i <= k; i++){
+                dp[i][count] = dp[i - 1][count - 1] + dp[i][count - 1] + 1;
             }
         }
-        return cnt;
+        return count;
     }
 }
