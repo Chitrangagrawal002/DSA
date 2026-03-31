51651bb5b08a476e3711365c9f6aed9a5f5d779a
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-03-31 20:23:54 +0530

    Time: 32 ms (75.43%) | Memory: 88.9 MB (91.43%) - LeetSync

---
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.java
@@ -0,0 +1,33 @@
+class Solution {
+    int n, m;
+    Integer[][] dp;
+    int mod = (int) 1e9 + 7;
+    int[][] dr = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int countPaths(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        dp = new Integer[n][m];
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                res = (res + dfs(grid, i, j)) % mod;
+            }
+        }
+        return res;
+    }
+    public int dfs(int[][] grid, int i, int j){
+        if(dp[i][j] != null) return dp[i][j];
+        int ans = 1;
+        for(int x = 0; x < 4; x++){
+            int nx = i + dr[x][0];
+            int ny = j + dr[x][1];
+            if(isSafe(nx, ny) && grid[nx][ny] < grid[i][j]){
+                ans = (ans + dfs(grid, nx, ny)) % mod;
+            }
+        }
+        return dp[i][j] = ans;
+    }
+    public boolean isSafe(int r, int c){
+        return r >= 0 && r < n && c >= 0 && c < m;
+    }
+}
