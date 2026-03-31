e3dc854f42da97c162878057bd99440ecbd5be5a
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-03-31 15:23:49 +0530

    Time: 61 ms (79.54%) | Memory: 43 MB (52.83%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getMaximumGold(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int max_ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                max_ans = Math.max(max_ans, helper(grid, i, j));
+            }
+        }
+        return max_ans;
+    }
+    public int helper(int[][] grid, int i, int j){
+        if(i >= grid.length || j >= grid[0].length || i < 0 || j < 0 || grid[i][j] == 0){
+            return 0;
+        }
+        int orig = grid[i][j];
+        grid[i][j] = 0;
+        int left = helper(grid , i, j - 1) + orig;
+        int right = helper(grid, i + 1, j) + orig;
+        int up = helper(grid, i - 1, j) + orig;
+        int down = helper(grid, i, j + 1) + orig;
+        grid[i][j] = orig;
+        return Math.max(left , Math.max(right, Math.max(up, down)));
+    }
+}
