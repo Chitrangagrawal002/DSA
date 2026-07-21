9124a81a483bad46a03b762f3da8e94cb1e7ebf3
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-07-21 10:06:49 +0530

    Add Books [B] from Codeforces

---
+++ 279/B - Books/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int left = 0, sum = 0, ans = 0;
+        for(int right = 0; right < n; right++){
+            sum += arr[right];
+            while(sum > t){
+                sum -= arr[left];
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        System.out.println(ans);
+    }
+}
