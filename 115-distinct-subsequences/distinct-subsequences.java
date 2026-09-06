ed0b787acbe5ec3f8afae082378ab49ab98fd6c8
Chitrangagrawal002 <chitrangagrawal002@gmail.com>
2026-09-06 13:08:57 +0530

    Time: 26 ms (9.00%) | Memory: 66.6 MB (6.38%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -0,0 +1,27 @@
+class Solution {
+    long dp[][];
+    public int numDistinct(String s, String t) {
+        dp = new long[s.length()][t.length()];
+        for(long[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return (int) solve(s, t, 0, 0);
+    }
+    public long solve(String s, String t, int i, int j){
+        if(j == t.length()){
+            return 1;
+        }
+        if(i == s.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        long ans = 0;
+        if(s.charAt(i) == t.charAt(j)){
+            ans += solve(s, t, i + 1, j + 1);
+        }
+        ans += solve(s, t, i + 1, j);
+        return dp[i][j] = ans;
+    }
+}
